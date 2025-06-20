ebbf540d948af0b0c332d83ca5daa60ceed7feb2
siddesh <siddhesh112004@gmail.com>
2025-06-20 14:28:47 +0530

    redux toolkit step for auth

---
+++ frontend/src/store/authSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    status: false,
+    role: null,
+    userdata: null,
+}
+
+const authSlice =createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+        login:(state, action) => {
+            state.status = true;
+            state.role = action.payload.role;
+            state.userdata = action.payload.userdata;
+        },
+        logout:(state) => {
+            state.status = false;
+            state.role = null;
+            state.userdata = null; 
+        }
+    }
+})
+
+export const {login, logout} = authSlice.actions;
+
+export default authSlice.reducer;
